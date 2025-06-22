efb193d3c08ffa2c1b8dce4be21dc48a6fbce977
Isaac <tuyishimeisaac17@gmail.com>
2025-06-22 10:39:27 +0200

    Initial commit

--- src/main/java/rw/ac/auca/ecommerce/core/base/AbstractAuditEntity.java
+++ src/main/java/rw/ac/auca/ecommerce/core/base/AbstractAuditEntity.java
@@ -3,7 +3,7 @@ package rw.ac.auca.ecommerce.core.base;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public abstract class AbstractAuditEntity {
+public abstract class AbstractAuditEntity  {
     private UUID id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
