2d4604c185ade7ce4caf1f8d658198e7fd8e688a
Isaac <tuyishimeisaac17@gmail.com>
2025-06-22 11:43:08 +0200

    Initial commit

--- src/main/java/rw/ac/auca/ecommerce/core/product/repository/ICustomerRepository.java
+++ src/main/java/rw/ac/auca/ecommerce/core/product/repository/ICustomerRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rw.ac.auca.ecommerce.core.customer.model.Customer;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ICustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByIdAndActive(UUID id, Boolean active);
 
 }
